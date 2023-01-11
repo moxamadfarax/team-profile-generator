e18054b0b6d098c8e55fe0968d663661741cac4c
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-01-11 15:30:25 -0800

    redid html gen

--- src/genHTML.js
+++ src/genHTML.js
@@ -1,82 +1,89 @@
-function managerCardGen() {
+function managerCardGen(name, role, id, email, officeNum) {
   return `      <div class="card">
   <div class="card-header">
     <ul>
-      <li><h2>${staffData[0].name}</h2></li>
+      <li><h2>${name}</h2></li>
       <br />
-      <li><h3>${staffData[0].role}</h3></li>
+      <li><h3>${role}</h3></li>
     </ul>
   </div>
   <div class="card-content">
     <ul class="container">
-      <li><h4>Employee ID : ${staffData[0].id}</h4></li>
+      <li><h4>Employee ID : ${id}</h4></li>
       <br />
       <br />
       <li>
         <h4>
           Email Adress :
-          <a href="mailto:${staffData[0].email}">email</a>
+          <a href="mailto:${email}">${email}</a>
         </h4>
       </li>
       <br />
       <br />
-      <li><h4>Office Number :</h4></li>
+      <li><h4>Office Number : ${officeNum}</h4></li>
     </ul>
   </div>
 </div>`;
 }
-function engineerCardGen() {
+function engineerCardGen(name, role, id, github) {
   return `      <div class="card">
   <div class="card-header">
     <ul>
-      <li><h2>Michael</h2></li>
+      <li>
+        <h2>${name}</h2>
+      </li>
       <br />
-      <li><h3>Intern</h3></li>
+      <li>
+        <h3>${role}</h3>
+      </li>
     </ul>
   </div>
   <div class="card-content">
     <ul class="container">
-      <li><h4>Employee ID :</h4></li>
+      <li>
+        <h4>Employee ID : ${id} </h4>
+      </li>
       <br />
       <br />
       <li>
         <h4>
-          Email Adress :
-          <a href="mailto:mohamedfarah0508@gmail.com">email</a>
+          Email Adress :<a href="mailto:${email}">${email}</a>
         </h4>
       </li>
       <br />
       <br />
-      <li><h4>School :</h4></li>
+      <li>
+        <h4>
+          <a href="https://github.com/${github}">${github}}</a>
+        </h4>
+      </li>
     </ul>
   </div>
 </div>`;
 }
-function internCardGen() {
+function internCardGen(name, role, id, school) {
   return `      <div class="card">
   <div class="card-header">
     <ul>
-      <li><h2>Sophia</h2></li>
+      <li><h2>${name}</h2></li>
       <br />
-      <li><h3>Engineer</h3></li>
+      <li><h3>${role}</h3></li>
     </ul>
   </div>
   <div class="card-content">
     <ul class="container">
-      <li><h4>Employee ID :</h4></li>
+      <li><h4>Employee ID : ${id}</h4></li>
       <br />
       <br />
       <li>
         <h4>
           Email Adress :
-          <a href="mailto:mohamedfarah0508@gmail.com">email</a>
+          <a href="mailto${email}">${email}</a>
         </h4>
       </li>
       <br />
       <br />
-      <li>
-        <h4><a href="https://github.com/moxamadfarax">github</a></h4>
-      </li>
+      <li><h4>School : ${school}</h4></li>
     </ul>
   </div>
 </div>`;
